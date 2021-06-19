3235dc9aca458da16c86d6b9182c6e22a58993ae
WMUT <WMUT.tech@gmail.com>
2021-06-19 22:31:15 +0900

    Cityを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_135850) do
+ActiveRecord::Schema.define(version: 2021_06_19_132652) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2021_06_16_135850) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "city_name"
+    t.string "city_code"
+    t.integer "pref_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pref_id"], name: "index_cities_on_pref_id"
   end
 
   create_table "drafts", force: :cascade do |t|
