4da6391163a778e5c1b89b74250a16f81bb5e2cb
WMUT <WMUT.tech@gmail.com>
2021-06-21 23:16:47 +0900

    Admin配下のModelを作成

---
+++ app/admin/prefs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Pref do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :pref_name, :pref_code
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:pref_name, :pref_code]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
